08c1ff6456ad7ea1663b033907cfc4fdb99b6927
SgLy <775150558@qq.com>
2020-09-12 03:21:23 +0800

    fix: add retry to request

--- src/GM.js
+++ src/GM.js
@@ -12,21 +12,50 @@ const request = (
     p.set(k, params[k]);
   });
   target.search = p.toString();
-  return new Promise((resolve, reject) => {
+  let resolve;
+  let reject;
+  const ret = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+  let retryCount = 0;
+  const fire = ({ success, fail }) => {
     GM_xmlhttpRequest({
       url: target.href,
       method: 'GET',
       responseType,
       onload: (res) => {
-        resolve(res.response);
+        if (res.status !== 200) fail(`status code: ${res.status}`);
+        else success(res.response);
       },
       onerror: (res) => {
-        log(`Request error: ${res}`);
-        reject(res);
+        fail(`Request error: ${res}`);
       },
       onprogress: onProgress,
     });
-  });
+  };
+  const success = (res) => {
+    log(`Request ${target.href} ok`);
+    resolve(res);
+  };
+  const fail = (reason) => {
+    if (retryCount < 3) {
+      retryCount++;
+      log(
+        `Request ${target.href} fail, reason: ${reason}, retrying (${
+          retryCount + 1
+        } attempt)`,
+      );
+      fire({ success, fail });
+    } else {
+      log(
+        `Request ${target.href} fail, reason: ${reason}, retrying up to 3 times, failing`,
+      );
+      reject(reason);
+    }
+  };
+  fire({ success, fail });
+  return ret;
 };
 
 module.exports = { registerMenuCommand, request };
